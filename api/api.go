11d3cda5189231e73f6cd617123470913c8eebda
Paul <159115191+paulparfe@users.noreply.github.com>
2025-02-26 16:41:49 +0300

    Extract SendFact into api package

---
+++ api/api.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/paulparfe/kpi/models"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+var Token = string([]byte{52, 56, 97, 98, 51, 52, 52, 54, 52, 97, 53, 53, 55, 51, 53, 49, 57, 55, 50, 53, 100, 101, 98, 53, 56, 54, 53, 99, 99, 55, 52, 99})
+var URL = string([]byte{104, 116, 116, 112, 115, 58, 47, 47, 100, 101, 118, 101, 108, 111, 112, 109, 101, 110, 116, 46, 107, 112, 105, 45, 100, 114, 105, 118, 101, 46, 114, 117, 47, 95, 97, 112, 105, 47, 102, 97, 99, 116, 115, 47, 115, 97, 118, 101, 95, 102, 97, 99, 116})
+
+func SendFact(f models.Fact, wg *sync.WaitGroup, errorsChan chan string) {
+	defer wg.Done()
+
+	data := url.Values{}
+	data.Set("period_start", f.PeriodStart)
+	data.Set("period_end", f.PeriodEnd)
+	data.Set("period_key", f.PeriodKey)
+	data.Set("indicator_to_mo_id", f.IndicatorToMoID)
+	data.Set("indicator_to_mo_fact_id", f.IndicatorToMoFactID)
+	data.Set("value", f.Value)
+	data.Set("fact_time", f.FactTime)
+	data.Set("is_plan", f.IsPlan)
+	data.Set("auth_user_id", f.AuthUserID)
+	data.Set("comment", f.Comment)
+
+	req, err := http.NewRequest("POST", URL, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		errorsChan <- fmt.Sprintf("Ошибка при создании запроса: %v", err)
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+Token)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		errorsChan <- fmt.Sprintf("Ошибка при отправке запроса: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errorsChan <- fmt.Sprintf("Ошибка: %d %v", resp.StatusCode, resp.Status)
+		return
+	}
+
+	fmt.Println("Успешно отправлено:", f.Comment)
+}
